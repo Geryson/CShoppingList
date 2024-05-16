611ed96f43e179bb1cf67f95c9ad47c593959c81
Csávás Gergely <csavas.g95@gmail.com>
2024-05-16 08:57:26 +0200

    MainActivity kinézetének előkészítése, kapcsolódó erőforrások elkészítése

--- app/src/main/java/com/gery/cshoppinglist/MainActivity.kt
+++ app/src/main/java/com/gery/cshoppinglist/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.gery.cshoppinglist
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +23,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setSupportActionBar(binding.tbMain)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() == R.id.action_delete_all) {
+            Toast.makeText(this, "Delete All", Toast.LENGTH_SHORT).show()
+            return true
+        } else {
+            return super.onOptionsItemSelected(item)
         }
     }
 }
