8b45c3b746fc3af9711d85a94e2e4086d302b268
Csávás Gergely <csavas.g95@gmail.com>
2024-05-16 11:32:44 +0200

    Listaelemek kinézetének optimalizálása, valuta ikon beillesztve

--- app/src/main/java/com/gery/cshoppinglist/adapter/ShoppingListAdapter.kt
+++ app/src/main/java/com/gery/cshoppinglist/adapter/ShoppingListAdapter.kt
@@ -3,6 +3,7 @@ package com.gery.cshoppinglist.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.gery.cshoppinglist.R
 import com.gery.cshoppinglist.data.ShoppingItem
@@ -12,7 +13,7 @@ import com.gery.cshoppinglist.databinding.ShoppingListItemLayoutBinding
 class ShoppingListAdapter : RecyclerView.Adapter<ShoppingListAdapter.ViewHolder> {
 
     private val items = mutableListOf(
-        ShoppingItem("Milk", "This is milk", 2, ShoppingItemCategory.FOOD, false),
+        ShoppingItem("Milk", "This is milk", 2, ShoppingItemCategory.FOOD, true),
         ShoppingItem("Smartphone", "This is smartphone", 220, ShoppingItemCategory.ELECTRONICS, false),
         ShoppingItem("Shirt", "This is shirt", 20, ShoppingItemCategory.CLOTHES, false)
     )
@@ -33,6 +34,14 @@ class ShoppingListAdapter : RecyclerView.Adapter<ShoppingListAdapter.ViewHolder>
 
         holder.shopItemBinding.btnItemDelete.setOnClickListener {
             deleteItem(holder.adapterPosition)
+        }
+
+        if (position % 2 == 0) {
+            holder.shopItemBinding.clItemContainer.setBackgroundColor(
+                ContextCompat.getColor(holder.itemView.context, R.color.list_item_background_even))
+        } else {
+            holder.shopItemBinding.clItemContainer.setBackgroundColor(
+                ContextCompat.getColor(context, R.color.list_item_background_odd))
         }
     }
 
@@ -58,7 +67,8 @@ class ShoppingListAdapter : RecyclerView.Adapter<ShoppingListAdapter.ViewHolder>
 
     inner class ViewHolder(val shopItemBinding: ShoppingListItemLayoutBinding): RecyclerView.ViewHolder(shopItemBinding.root) {
         fun bind(shopItem: ShoppingItem) {
-            shopItemBinding.tvItemName.text = shopItem.name
+            val formattedName = shopItem.name + ","
+            shopItemBinding.tvItemName.text = formattedName
             shopItemBinding.tvItemPrice.text = shopItem.price.toString()
             shopItemBinding.tvItemDescription.text = shopItem.description
             shopItemBinding.cbItemStatus.isChecked = shopItem.status
