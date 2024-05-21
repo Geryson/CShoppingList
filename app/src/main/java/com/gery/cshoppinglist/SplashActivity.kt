739ba81e39ccc03cddabc19ca34208c5ef8bd2fc
Csávás Gergely <csavas.g95@gmail.com>
2024-05-21 09:53:27 +0200

    SplashActivity animáció elkészítése

--- app/src/main/java/com/gery/cshoppinglist/SplashActivity.kt
+++ app/src/main/java/com/gery/cshoppinglist/SplashActivity.kt
@@ -4,17 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.gery.cshoppinglist.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -24,5 +31,9 @@ class SplashActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 3000)
+
+        val splashIconAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_icon_anim)
+        binding.ivSplashIcon.startAnimation(splashIconAnimation)
+
     }
 }
